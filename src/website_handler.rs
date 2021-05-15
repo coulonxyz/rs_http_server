b5686768a7134e6bbe946825ba86428433dfe738
Julien Coulon <julien.coulon@gmail.com>
2021-05-15 19:34:15 +0200

    chore: add test to website_handler

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -2,6 +2,7 @@ use super::server::Handler;
 use crate::http::{Request, Response, StatusCode, Method};
 use std::fs;
 
+#[derive(Debug)]
 pub struct WebsiteHandler {
     public_path: String
 }
@@ -40,5 +41,28 @@ impl Handler for WebsiteHandler {
             }
             _ => Response::new(StatusCode::NotFound, Some("<h1>404</h1>".to_string())),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn read_file_test_html() {
+        let default_path = format!("{}/test_fixtures", env!("CARGO_MANIFEST_DIR"));
+        let handler = WebsiteHandler::new(default_path);
+        assert_eq!(handler.read_file("test.html"), Some("<html lang=\"en\"></html>\n".to_string()));
+    }
+
+    #[test]
+    fn none_if_directory_reversal_attack() {
+        let handler = WebsiteHandler::new("some_path".to_string());
+        assert_eq!(handler.read_file("../Cargo.toml"), None);
+    }
+
+    #[test]
+    fn none_if_invalid_path() {
+        let handler = WebsiteHandler::new("some_path".to_string());
+        assert_eq!(handler.read_file("some invalid path 😄"), None);
     }
 }
