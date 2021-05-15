4788aa32c6fcd739608f7aa444d157b5e6a46797
Julien Coulon <julien.coulon@gmail.com>
2021-05-15 17:56:04 +0200

    chore: initial commit

---
+++ src/http/query_string.rs
@@ -0,0 +1,45 @@
+use std::collections::HashMap;
+
+#[derive(Debug)]
+pub struct QueryString<'buf> {
+    data: HashMap<&'buf str, Value<'buf>>
+}
+
+#[derive(Debug)]
+pub enum Value<'buf> {
+    Single(&'buf str),
+    Multiple(Vec<&'buf str>),
+}
+
+impl<'buf> QueryString<'buf> {
+    pub fn get(&self, key: &str) -> Option<&Value> {
+        self.data.get(key)
+    }
+}
+
+impl<'buf> From<&'buf str> for QueryString<'buf> {
+    fn from(s: &'buf str) -> Self {
+        let mut data = HashMap::new();
+
+        for sub_str in s.split('&') {
+            let mut key = sub_str;
+            let mut value = "";
+            if let Some(i) = sub_str.find('=') {
+                key = &sub_str[..i];
+                value = &sub_str[i + 1..];
+            }
+            data.entry(key)
+                .and_modify(|existing_value: &mut Value| match existing_value {
+                    Value::Single(previous_value) => {
+                        *existing_value = Value::Multiple(vec![previous_value, value]);
+                    }
+                    Value::Multiple(previous_vec) => {
+                        previous_vec.push(value);
+                    }
+                })
+                .or_insert(Value::Single(value));
+
+        }
+        QueryString { data }
+    }
+}
